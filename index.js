0ffd6c279780579aa1220e99550ddaad4d6afa02
Jeff146354 <jeff146354tristan@apps.ipb.ac.id>
2025-05-15 21:06:33 +0700

    Update index.js
    const bcrypt = require('bcryptjs');

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const mysql = require('mysql2');
 const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 const app = express();
 app.use(cors());
